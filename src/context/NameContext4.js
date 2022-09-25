a66ac2e6fd57c63dd114c9554688879040def50f
ColorFilter <johan@romantech.net>
2022-09-25 21:56:47 +0900

    :adhesive_bandage: Add reducer default condition (#5)

--- src/context/NameContext4.js
+++ src/context/NameContext4.js
@@ -19,7 +19,7 @@ const nameContextReducer = (state, action) => {
     case SET_FIRST_NAME:
       return { ...state, firstName: action.payload };
     default:
-      return state;
+      throw new Error(`Unhandled action type: ${action.type}`);
   }
 };
 
